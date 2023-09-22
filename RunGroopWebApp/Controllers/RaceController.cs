8d4b261afed555c99814d7c4bd3eec5c149b6c3f
fakeITDevTeam <dev5544455@gmail.com>
2023-09-22 10:14:46 +0700

    upload photos to cloud

--- RunGroopWebApp/Controllers/RaceController.cs
+++ RunGroopWebApp/Controllers/RaceController.cs
@@ -3,16 +3,21 @@ using Microsoft.EntityFrameworkCore;
 using RunGroopWebApp.Data;
 using RunGroopWebApp.Interfaces;
 using RunGroopWebApp.Models;
+using RunGroopWebApp.Repository;
+using RunGroopWebApp.Services;
+using RunGroopWebApp.ViewModels;
 
 namespace RunGroopWebApp.Controllers
 {
     public class RaceController : Controller
     {
         public IRaceRepository _raceRepository;
+        public IPhotoService _photoService;
 
-        public RaceController(ApplicationDbContext context, IRaceRepository raceRepository)
+        public RaceController(IRaceRepository raceRepository, IPhotoService photoService)
         {            
             _raceRepository = raceRepository;
+            _photoService = photoService;
         }
 
         public async Task<IActionResult> Index()
@@ -27,6 +32,42 @@ namespace RunGroopWebApp.Controllers
             Race race = await _raceRepository.GetByIdAsync(id);
 
             return View(race);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateRaceViewModel raceVM)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await _photoService.AddPhotoAsync(raceVM.Image);
+
+                var race = new Race
+                {
+                    Title = raceVM.Title,
+                    Description = raceVM.Description,
+                    Image = result.Url.ToString(),
+                    Address = new Address
+                    {
+                        Street = raceVM.Address.Street,
+                        City = raceVM.Address.City,
+                        State = raceVM.Address.State
+                    }
+                };
+                _raceRepository.Add(race);
+
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                ModelState.AddModelError("", "Photo upload failed");
+            }
+
+            return View(raceVM);
         }
     }
 }
--- RunGroopWebApp/Program.cs
+++ RunGroopWebApp/Program.cs
@@ -1,7 +1,9 @@
 using Microsoft.EntityFrameworkCore;
 using RunGroopWebApp.Data;
+using RunGroopWebApp.Helpers;
 using RunGroopWebApp.Interfaces;
 using RunGroopWebApp.Repository;
+using RunGroopWebApp.Services;
 
 namespace RunGroopWebApp
 {
@@ -15,6 +17,8 @@ namespace RunGroopWebApp
             builder.Services.AddControllersWithViews();
             builder.Services.AddScoped<IClubRepository, ClubRepository>();
             builder.Services.AddScoped<IRaceRepository, RaceRepository>();
+            builder.Services.AddScoped<IPhotoService, PhotoService>();
+            builder.Services.Configure<CloudinarySettings>(builder.Configuration.GetSection("CloudinarySettings"));
             builder.Services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
             );
