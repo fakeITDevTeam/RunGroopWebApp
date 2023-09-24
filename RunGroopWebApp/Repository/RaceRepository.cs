92b0feb6ad7f10faa67bdd3eb82d394e519664e8
fakeITDevTeam <dev5544455@gmail.com>
2023-09-24 19:12:50 +0700

    add delete page

--- RunGroopWebApp/Repository/RaceRepository.cs
+++ RunGroopWebApp/Repository/RaceRepository.cs
@@ -45,7 +45,8 @@ namespace RunGroopWebApp.Repository
 
         public async Task<Race> GetByIdAsyncNoTracking(int id)
         {
-            return await _context.Races.Include(i => i.Address).AsNoTracking().FirstOrDefaultAsync(i => i.Id == id);
+            //return await _context.Races.Include(i => i.Address).AsNoTracking().FirstOrDefaultAsync(i => i.Id == id);
+            return await _context.Races.Include(i => i.Address).AsNoTracking().FirstOrDefaultAsync();
         }
 
         public bool Save()
